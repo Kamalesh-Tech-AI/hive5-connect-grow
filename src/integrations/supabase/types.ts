89cd370737f4763baa877acd34af926e8ade9d1e
Kamalesh <kamalesh3703s@gmail.com>
2025-08-07 08:27:30 +0100

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -14,6 +14,42 @@ export type Database = {
   }
   public: {
     Tables: {
+      contact_messages: {
+        Row: {
+          created_at: string
+          email: string
+          first_name: string
+          id: string
+          last_name: string
+          message: string
+          status: string
+          subject: string
+          updated_at: string
+        }
+        Insert: {
+          created_at?: string
+          email: string
+          first_name: string
+          id?: string
+          last_name: string
+          message: string
+          status?: string
+          subject: string
+          updated_at?: string
+        }
+        Update: {
+          created_at?: string
+          email?: string
+          first_name?: string
+          id?: string
+          last_name?: string
+          message?: string
+          status?: string
+          subject?: string
+          updated_at?: string
+        }
+        Relationships: []
+      }
       editing_enrollments: {
         Row: {
           amount: number
